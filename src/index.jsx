e03260bc65314a168c38ad264f4f7b3e60228571
valentinaAlfonso <valfonso@sparkdigital.com>
2017-05-02 11:25:59 -0300

    Mobile first structure

--- src/index.jsx
+++ src/index.jsx
@@ -8,34 +8,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
-var ContactItem = React.createClass({
-  propTypes: {
-    name: React.PropTypes.string.isRequired,
-  },
-
-  render: function() {
-    return (
-      React.createElement('li', {className: 'Contact'},
-        React.createElement('h2', {className: 'Contact-name'}, this.props.name)
-      )
-    )
-  },
-});
-
-var element = React.createElement(ContactItem, {name: "Spark Digital Employee"});
-
-var rootElement =
+var headerElement =
   React.createElement('div', {}, 
-    React.createElement('h1', {}, "USer Info"),
-    React.createElement('ul', {},
-      React.createElement('li', {},
-        React.createElement('h2', {href: '/perfil'}, "perfil"),
-        React.createElement('a', {href: '/info-beneficio'}, 'Informacion sobre los masajes')
-      )
-    )
-  ) 
+    React.createElement('h1', {}, "User options")
+  );
+var mainElement =
+  React.createElement('div', {}, 
+    React.createElement('h1', {}, "Calendar")
+  );
+  
+var footerElement =
+  React.createElement('div', {}, 
+    React.createElement('h1', {}, "Next day ->")
+  );
 
-ReactDOM.render(element, document.getElementById('react-app2'));
-ReactDOM.render(rootElement, document.getElementById('react-app'));
-ReactDOM.render(<App />, document.getElementById('react-root'));
+ReactDOM.render(headerElement, document.getElementById('header-container'));
+ReactDOM.render(mainElement, document.getElementById('main-container'));
+ReactDOM.render(footerElement, document.getElementById('footer-container'));
 
